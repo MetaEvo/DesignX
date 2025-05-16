2de0a515a95602fc99654b74576c649627b548b5
GHS <54709287+AtWillTeam@users.noreply.github.com>
2025-05-16 11:10:19 +0800

    Update critic_network.py

--- nets/critic_network.py
+++ nets/critic_network.py
@@ -1,9 +1,4 @@
 import os
-os.environ['OMP_NUM_THREADS'] = '1'
-os.environ['OPENBLS_NUM_THREADS'] = '1'
-os.environ['GOTO_NUM_THREADS'] = '1'
-os.environ['MKL_NUM_THREADS'] = '1'
-os.environ['TORCH_NUM_THREADS'] = '1'
 from torch import nn
 from nets.graph_layers import MLP
 import torch
